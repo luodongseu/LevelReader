043539c37e09465a4884e3d7b1071290c1af8029
luodongseu <1576848284@qq.com>
2015-05-21 00:13:34 +0800

    Add a new Android Project

---
+++ LevelReader/src/com/dts/app/levelreader/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dts.app.levelreader.activity;
+
+import com.dts.app.levelreader.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/***
+ * LevelReader
+ * 
+ * @author LD
+ * @date 2015-5-20
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
